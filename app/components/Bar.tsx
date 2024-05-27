aa3a86727a5cd0e3a37e9fa04634a4a6502dc991
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-05-27 09:17:41 +0100

    fixing chaart

--- app/components/Bar.tsx
+++ app/components/Bar.tsx
@@ -1,11 +1,30 @@
-import { ChartOptions } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartOptions,
+} from "chart.js";
 import { Bar } from "react-chartjs-2";
+
+// Register the necessary Chart.js components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const options: ChartOptions<"bar"> = {
   responsive: true,
   plugins: {
     legend: {
-      position: "top" as const, // Use 'as const' to assert the type
+      position: "top",
     },
     title: {
       display: true,
@@ -25,8 +44,8 @@ const data = {
     {
       label: "Dataset 1",
       data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: "rgba(75,192,192,0.2)",
-      borderColor: "rgba(75,192,192,1)",
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      borderColor: "rgba(75, 192, 192, 1)",
       borderWidth: 1,
     },
   ],
